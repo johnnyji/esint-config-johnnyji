1f6268846dda6920a349158efdea88417611470c
Johnny Ji <johnny@johnnyji.com>
2016-05-08 22:53:11 -0700

    added more rules

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ module.exports = {
   'rules': {
     // Never put spaces in opening/closing brackets of arrays
     'array-bracket-spacing': [2, 'never'],
+    'arrow-spacing': 2,
     'babel/arrow-parens': [2, 'always'],
     'babel/new-cap': 0,
     'babel/object-curly-spacing': [2, 'never'],
@@ -34,7 +35,7 @@ module.exports = {
     'comma-spacing': [2, {'before': false, 'after': true}],
     // Commas between properties must end on the same line, never begin the new line
     'comma-style': [2, 'last'],
-    // Does not allow spacing inside object property accessors: `obj[ a ]` is incorrect
+    // Does not allow spacing inside object property accessors: use `obj[a]`, not `obj[ a ]`
     'computed-property-spacing': [2, 'never'],
     'constructor-super': 2,
     // Multi-line chaining must start new-line chained properties with a dot
@@ -42,14 +43,16 @@ module.exports = {
     // Must use triple equality check, except for checking null/undefined
     'eqeqeq': [2, 'allow-null'],
     'indent': [2, 2, {'SwitchCase': 1}],
+    'key-spacing': [2, {'beforeColon': false, 'afterColon': true, 'mode': ' strict'}],
     'linebreak-style': [2, 'unix'],
+    'no-array-constructor': 2,
     'no-const-assign': 2,
     // No `else` statement needed if `if` statement returns (Guard clause)
     'no-else-return': 2,
     'no-extra-label': 2,
-    // Must prefix all floats with `0` if it is a decimal property: `.15` is incorrect
+    // Must prefix all floats with `0` if it is a decimal property: use `0.15`, not `.15`
     'no-floating-decimal': 2,
-    // Must convert value types explicitly: use `String(foo)` instead of `"" + foo'
+    // Must convert value types explicitly: use `String(foo)`, not `"" + foo`
     'no-implicit-coercion': 2,
     // Cannot use `__iterator__` property on objects
     'no-iterator': 2,
@@ -60,37 +63,52 @@ module.exports = {
     'no-multi-spaces': 2,
     'no-native-assign': 2,
     'no-new': 2,
+    // Cannot do `new Function(...)`
     'no-new-func': 2,
+    'no-new-object': 2,
+    // Cannot do `new (require('path/to/some/file'))`
     'no-new-require': 2,
     'no-new-wrappers': 2,
     // Cannot reassign function parameters
     'no-param-reassign': 2,
     // Cannot use `__proto__` property on objects
     'no-proto': 2,
+    // Disallows the use of `++` and `--` operators
+    'no-plusplus': 2,
     'no-redeclare': 2,
     'no-return-assign': 2,
     'no-scrip-url': 2,
     'no-self-assign': 2,
     'no-self-compare': 2,
     'no-sequences': 2,
+    // No space between function name and brackets when invoking: use `myFunc()`, not `myFunc ()`
+    'no-spaced-func': 2,
     // No trailing spaces on lines
     'no-trailing-spaces': [2, {'skipBlankLines': true}],
     // Must always modify loops to prevent infinite looping
     'no-unmodified-loop-condition': 2,
+    // Disallows ternary operators when unneccesary
+    'no-uneeded-ternary': 2,
     'no-unused-expressions': 2,
     // No empty `constructor()` in classes
     'no-useless-constructor': 2,
+    // Cannot use the `with` statement
     'no-with': 2,
-    'object-shorthand': 2,
+    'object-shorthand': [2, 'always'],
+    // Disallows multiple variable declarations on one line
     'one-var-declaration-per-line': 2,
     'prefer-arrow-callback': 2,
     'prefer-const': 2,
+    'prefer-rest-params',
     'prefer-spread': 2,
     'prefer-template': 2,
+    // Only use single quotes and backticks except for when the string itself contains single quotes
     'quotes': [2, 'single', {'avoidEscape': true, 'allowTemplateLiterals': true}],
+    // Enforces `key` prop on iterable React elements
     'react/jsx-key': 2,
     'react/jsx-max-props-per-line': [2, {'maximum': 4}],
     'react/jsx-no-duplicate-props': [2, {'ignoreCase': true}],
+    // Always leave a space before closing JSX tag: use `<Hello />`, not `<Hello/>
     'react/jsx-space-before-closing': [2, 'always'],
     'react/no-did-mount-set-state': 2,
     'react/no-did-update-set-state': 2,
@@ -98,6 +116,7 @@ module.exports = {
     'react/no-is-mounted': 2,
     'react/no-unknown-property': 2,
     'semi': [2, 'always'],
+    'spaced-comment': [2, 'always'],
     'strict': [2, 'global'],
     'yoda': [2, 'always']
   }
